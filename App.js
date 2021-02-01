b52e036af42029cfe1d73b7c556a22fa2de56ccb
David Paez <dpaez013@fiu.edu>
2021-02-01 14:58:31 -0500

    Adding logo and image component example

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require('./assets/pet_care.png')}
+        style={{ width: 200, height: 200 }}
+      />
+      <Text>App FINISHED!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
